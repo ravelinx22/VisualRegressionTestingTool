6dbf155564f6c066ede987e7c21fa86a70e64dbd
William Ravelo <drummerwilliam@gmail.com>
2018-09-17 23:04:50 -0500

    testing

--- server/routes/test.js
+++ server/routes/test.js
@@ -8,7 +8,7 @@ router.get("/", function(req, res) {
 });
 
 const takeScreenshot = async(url) => {
-	const browser = await puppeteer.launch();
+	const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
 	const page = await browser.newPage();
 	await page.goto(url);
 	await page.evaluate(() => {
