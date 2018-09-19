5aae3da7631fdf685b8971e9d45eb1ab68dbcf1b
William Ravelo <drummerwilliam@gmail.com>
2018-09-19 18:45:25 -0500

    added description

--- client/components/report_row.jsx
+++ client/components/report_row.jsx
@@ -12,7 +12,46 @@ export default class ReportRow extends Component {
 	componentDidMount() {
 	}
 
+	renderDimensionDifferenceWidth() {
+		let data =	{
+			misMatchPercentage : 100, // %
+			isSameDimensions: true, // or false
+			dimensionDifference: { width: 0, height: -1  }, // defined if dimensions are not the same
+			getImageDataUrl: function(){}		
+		}
+		let difference = data.dimensionDifference;
+		if(difference && difference.width != undefined) {
+			return(
+				<div><strong>Width Difference:</strong> {difference.width}</div>
+			);
+		}
+		return null;
+	}
+
+	renderDimensionDifferenceHeight() {
+		let data =	{
+			misMatchPercentage : 100, // %
+			isSameDimensions: true, // or false
+			dimensionDifference: { width: 0, height: -1  }, // defined if dimensions are not the same
+			getImageDataUrl: function(){}		
+		}
+		let difference = data.dimensionDifference;
+		if(difference && difference.height != undefined) {
+			return(
+				<div><strong>Height Difference:</strong> {difference.height}</div>
+			);
+		}
+		return null;
+	}
+
 	render() {
+		let data =	{
+			misMatchPercentage : 100, // %
+			isSameDimensions: true, // or false
+			dimensionDifference: { width: 0, height: -1  }, // defined if dimensions are not the same
+			getImageDataUrl: function(){}		
+		}
+
 		return (
 			<Row className="reportRow">
 				<Col className="dateEntry" md="2">Domingo, 16 de septiembre de 2018</Col>
@@ -26,7 +65,10 @@ export default class ReportRow extends Component {
 					<img className="testImage" src="./img/tests/diff.png" alt="test"/>
 				</Col>
 				<Col className="infoEntry" md="2">
-					Informacion importante
+					<div><strong>Mismatch Percentage:</strong> {data.misMatchPercentage}</div>
+					<div><strong>Is Same Dimensions:</strong> {data.isSameDimensions + ""}</div>
+					{this.renderDimensionDifferenceWidth()}
+					{this.renderDimensionDifferenceHeight()}
 				</Col>
 			</Row>
 		);
